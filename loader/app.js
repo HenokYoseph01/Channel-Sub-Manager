00a61cf80afb4b5c208eca0f2e99eed911985b17
Henok Yoseph <henok.y2014@gmail.com>
2025-04-18 22:21:21 +0300

    fix(server): Add server listen event to express file

--- loader/app.js
+++ loader/app.js
@@ -1,12 +1,21 @@
 require("dotenv").config(".env");
+const express = require('express')
 const cron = require("node-cron");
 const {checkAndPostUpdate} = require('../utils/checkAndUpdate');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
 
 const loader = () => {
   //Set up Scheduler
   cron.schedule("0 23 * * *", () => {
     checkAndPostUpdate();
   });
+
+  app.listen(PORT, ()=>{
+    console.log(`SERVER RUNNING ON PORT: ${PORT}`);
+  })
 };
 
 module.exports = loader
